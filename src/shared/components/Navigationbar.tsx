b89c621df66d75addfa7e00523f2a4cd3109d774
Óscar Miranda <oscarmiranda3615@gmail.com>
2021-11-01 16:12:34 -0600

    feat: display active link to navbar

--- src/shared/components/Navigationbar.tsx
+++ src/shared/components/Navigationbar.tsx
@@ -2,9 +2,12 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import config from 'shared/config';
 
 const NavigationBar = () => {
+	const { pathname } = useRouter();
+
 	return (
 		<Navbar bg="primary" variant="dark" expand="lg">
 			<Container>
@@ -15,10 +18,16 @@ const NavigationBar = () => {
 				<Navbar.Collapse id="navbar-nav">
 					<Nav className="ms-auto">
 						<Link href="/" passHref>
-							<Nav.Link>Blog</Nav.Link>
+							<Nav.Link className={pathname === '/' ? 'active' : undefined}>
+								Blog
+							</Nav.Link>
 						</Link>
 						<Link href="/about" passHref>
-							<Nav.Link>Acerca de</Nav.Link>
+							<Nav.Link
+								className={pathname === '/about' ? 'active' : undefined}
+							>
+								Acerca de
+							</Nav.Link>
 						</Link>
 					</Nav>
 				</Navbar.Collapse>
