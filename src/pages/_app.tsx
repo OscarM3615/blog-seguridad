8e6f9cf3889bc316ae986cb476de5e5b69f5b0cf
Óscar Miranda <oscarmiranda3615@gmail.com>
2021-11-05 20:46:05 -0600

    feat: update font family to Roboto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,6 +15,17 @@ const App = ({ Component, pageProps }: AppProps) => {
 					rel="stylesheet"
 					href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
 				/>
+
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link
+					rel="preconnect"
+					href="https://fonts.gstatic.com"
+					crossOrigin=""
+				/>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 
 			<NavigationBar />
